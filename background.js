ce4325381cf96b4e13f49ad12e7d593a5972aded
darthvader2099-eng <inihgrebmal@gmail.com>
2025-08-02 17:00:02 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  const targetURL = "https://chat.openai.com"; // Change this to your desired tab
+  const targetURL = "https://chatgpt.com"; // Change this to your desired tab
 
   if (changeInfo.status === "complete" && tab.url.startsWith(targetURL)) {
     chrome.scripting.executeScript({
@@ -11,3 +11,4 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     });
   }
 });
+
